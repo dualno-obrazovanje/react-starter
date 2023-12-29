4c08b4dbbe9f71dd41f5e07cc2efd8e346c00da3
Jovan Medarovic <end54065@adobe.com>
2023-12-29 14:18:03 +0100

    14th lesson

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import './style.css';
+
+const App = () => <h1>Test</h1>;
+
+export default App;
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: {
+    hot: true,
+    open: true,
+  },
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+}
