a259b995cd7eb895cf2c575cd75e05e3b01e4262
Jovan Medarovic <end54065@adobe.com>
2024-02-02 13:54:01 +0100

    sass implementation

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -18,8 +18,12 @@ module.exports = {
         ],
       },
       {
-        test: /\.css?$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
